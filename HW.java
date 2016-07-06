a0c460c2ef3a84ce99be8af0fc6167eaff27045a
Gautam Sharma <gautam.sharma@datametica.com>
2016-07-06 16:09:40 +0530

    added

--- HW.java
+++ HW.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<<<<<<< HEAD is not here
 c class HelloWorld { public static void main(String[] args) { System.out.println("Hello World"); } }
 =======
 public class HelloWorld { public static void main(String[] args) { System.out.println("Hello World"); } } 
